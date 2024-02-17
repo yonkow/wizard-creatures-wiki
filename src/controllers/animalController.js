403254428d770490957ce8f8607880ad464d9193
yonkow <42253121+yonkow@users.noreply.github.com>
2024-02-17 17:16:46 +0200

    Add post to create an animal

--- src/controllers/animalController.js
+++ src/controllers/animalController.js
@@ -1,7 +1,30 @@
+const { isAuth } = require('../middlewares/authMiddleware');
+const { errorMessenger } = require('../utils/errorMessageUtil');
 const router = require('express').Router();
+const animalService = require('../services/animalService');
 
-router.get('/create', (req, res) => {
-    res.render('animals/create');
+router.get('/', (req, res) => {
+
+
+    res.render('animals/all-posts');
 })
+
+router.get('/create', isAuth, (req, res) => {
+    res.render('animals/create');
+});
+
+router.post('/create', isAuth, async (req, res) => {
+    const animalData = req.body;
+    animalData.owner = req.user._id;
+
+    try {
+        await animalService.create(animalData);
+
+        res.redirect('/animals');
+
+    } catch (err) {
+        res.render('animals/create', { ...animalData, error: errorMessenger(err) })
+    };
+});
 
 module.exports = router;
