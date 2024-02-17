d0a5c2aef4d82d36144618d5722dd6e2146bd432
yonkow <42253121+yonkow@users.noreply.github.com>
2024-02-17 18:01:58 +0200

    Updated animal service

--- src/services/animalService.js
+++ src/services/animalService.js
@@ -1,5 +1,10 @@
 const Animal = require('../models/Animal')
+const User = require('../models/User');
 
 exports.getAll = () => Animal.find();
 
-exports.create = (animalData) => Animal.create(animalData);
+exports.create = async (animalData) => {
+    const animal = await Animal.create(animalData);
+
+    await User.findByIdAndUpdate(animalData.owner, { $push: { createdAnimals: animal._id } });
+};
